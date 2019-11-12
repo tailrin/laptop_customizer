5d0c67457ab5220e33402543b74fd70118136dbd
josh <tailrin.jh@gmail.com>
2019-11-12 08:17:58 -0700

    added summaryItem component

--- src/Summary/Summary.js
+++ src/Summary/Summary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Summary.css';
+import SummaryItem from './SummaryItem/SummaryItem.js'
 
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -15,16 +16,8 @@ class Summary extends React.Component{
         const summary = Object.keys(this.props.selected).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
             const selectedOption = this.props.selected[feature];
-      
-            return (
-              <div className="summary__option" key={featureHash}>
-                <div className="summary__option__label">{feature} </div>
-                <div className="summary__option__value">{selectedOption.name}</div>
-                <div className="summary__option__cost">
-                  {USCurrencyFormat.format(selectedOption.cost)}
-                </div>
-              </div>
-            );
+
+            return <SummaryItem featureHash={featureHash} feature={feature} selectedOption={selectedOption}/>
         });
         const total = Object.keys(this.props.selected).reduce(
             (acc, curr) => acc + this.props.selected[curr].cost,
---
+++ src/Summary/SummaryItem/SummaryItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+class SummaryItem extends React.Component{
+    static defaultProps = {
+        featureHash: "",
+        feature: "",
+        selectedOption: {
+            name: "",
+            cost: 0
+        },
+    }
+
+    render(){
+        return (
+            <div className="summary__option" key={this.props.featureHash}>
+              <div className="summary__option__label">{this.props.feature} </div>
+              <div className="summary__option__value">{this.props.selectedOption.name}</div>
+              <div className="summary__option__cost">
+                {USCurrencyFormat.format(this.props.selectedOption.cost)}
+              </div>
+            </div>
+        );
+    }
+}
+
+export default SummaryItem;
